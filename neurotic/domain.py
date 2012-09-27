be73474d19106ac0e72f58d4ee57e61959daa402
Felipe cruz <felipecruz@loogica.net>
2012-09-27 13:44:37 -0300

    add id to each report

--- neurotic/domain.py
+++ neurotic/domain.py
@@ -5,12 +5,17 @@ from coopy.decorators import readonly
 class TestReportRepository(object):
     def __init__(self):
         self.reports = []
+        self.counter = 0
+        self.last = 0
 
     def add_report(self, report):
+        self.counter += 1
+        report['id'] = self.counter
         self.reports.append(report)
 
     @readonly
-    def show_history(self, size=10):
-        import json
-        for report in self.reports[:size]:
+    def show_history(self, size=1):
+        data =  self.reports[self.last:]
+        self.last = len(self.reports)
+        for report in data:
             print(json.dumps(report, indent=4))
