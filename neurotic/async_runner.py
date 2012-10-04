e642261835cee732ac4e57b3989e6578ef175cf2
Felipe cruz <felipecruz@loogica.net>
2012-10-04 17:35:12 -0300

    remove print and fix identation

--- neurotic/async_runner.py
+++ neurotic/async_runner.py
@@ -3,6 +3,7 @@ import subprocess
 
 def async_test(command):
     os.chdir(os.getcwd())
-    print("Running %s on %s" % (command, os.getcwd()))
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
     out, err = p.communicate()
