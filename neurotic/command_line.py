78e15c3fd2ec65a214ee49bf02e73e7c797cbaa9
Felipe cruz <felipecruz@loogica.net>
2012-10-05 00:23:56 -0300

    fix command line tasks and remove '-' from output

--- neurotic/command_line.py
+++ neurotic/command_line.py
@@ -9,31 +9,30 @@ repository = init_persistent_system(TestReportRepository,
                                     basedir="build_history")
 
 def main():
-    print("Searching build history on %s" % (os.getcwd()))
     mod = __import__(__name__).command_line
     if len(sys.argv) == 1:
         show_last_run()
     else:
         function = getattr(mod, sys.argv[1])(sys.argv[2:])
 
-def web():
+def web(paths):
     from neurotic.wsgi_app import app
     app.run(debug=True)
 
-def console_history():
+def console_history(paths):
     repository.show_history()
 
-def last_fails():
+def last_fails(paths):
     for failed in repository.last_run_failed_tests():
         print(failed)
 
 def show_last_run():
     for report in repository.last_run_report_type():
         if report['outcome'] == "failed":
-            print("%s - %s" % (color(ERROR, color="red"),
+            print("%s %s" % (color(ERROR, color="red"),
                                    report['nodeid']))
         else:
-            print("%s- %s" % (color(OK, color="green"),
+            print("%s %s" % (color(OK, color="green"),
                               report['nodeid']))
 
 
