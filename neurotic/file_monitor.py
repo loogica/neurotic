db83edb618992f6a0d8b9fbbd4838c8819ba531b
Felipe cruz <felipecruz@loogica.net>
2012-12-14 14:28:34 -0200

    minimize unecessary directory walks checking for
    modifications only on user configured paths.
    
    Print stdout in case of test run ERROR (compilation
    error for instance).
    
    Ignore events occurring with less than 1 second
    time difference.

--- neurotic/file_monitor.py
+++ neurotic/file_monitor.py
@@ -10,11 +10,12 @@ from select import kqueue, kevent
 
 from coopy.base import init_persistent_system
 
-from neurotic.async_runner import async_test
+from neurotic.async_runner import async_test, NeuroticError
 
 last_scan_dict = {}
+LIMIT = 1
 
-def check_modifications(dirpath):
+def check_modifications(dirpath, paths):
     modifications = False
     local_scan_dict = {}
     global last_scan_dict
@@ -22,13 +23,15 @@ def check_modifications(dirpath):
     entries = glob.glob(os.path.join(dirpath, "*.py"))
 
     for dirname, dirnames, filenames in os.walk(dirpath):
-        for subdirname in dirnames:
-            files = glob.glob(os.path.join(os.path.abspath(subdirname), '*.py'))
+        if dirname in paths:
+            files = glob.glob(os.path.join(os.path.abspath(dirname), '*.py'))
             if files:
                 entries.extend(files)
 
     entries = ((time.ctime(os.stat(path).st_mtime), path)
                 for path in entries if os.path.isfile(path))
+
+    entries = set(entries)
 
     for cdate, path in sorted(entries):
         local_scan_dict[os.path.basename(path)] = cdate
@@ -40,7 +43,9 @@ def check_modifications(dirpath):
     return modifications
 
 def start_monitor(dirs):
+    last_run = time.time()
     files_stats = []
+    paths = []
     current_dir = os.getcwd()
 
     kq = kqueue()
@@ -48,6 +53,7 @@ def start_monitor(dirs):
     source_events = []
     for dir_name in dirs:
         dir_path = current_dir + '/' + dir_name
+        paths.append(dir_path)
         fd = os.open(dir_path, os.O_RDONLY)
         event = kevent(fd, filter=select.KQ_FILTER_VNODE,
                         flags=select.KQ_EV_ADD | select.KQ_EV_CLEAR,
@@ -56,12 +62,18 @@ def start_monitor(dirs):
 
     while True:
         events = kq.control(source_events,  len(source_events), 2000)
-        for event in events:
-            if event.fflags & select.KQ_NOTE_WRITE:
-                if check_modifications(current_dir):
-                    try:
-                        async_test(["make", "test"])
-                    except:
-                        pass
+        if any(map(lambda e: e.fflags & select.KQ_NOTE_WRITE, events)):
+            if (time.time() - last_run) < LIMIT:
+                continue
+            if check_modifications(current_dir, paths):
+                try:
+                    async_test(["make", "test"])
                     os.system('clear')
                     subprocess.Popen("neurotic")
+                except NeuroticError as ne:
+                    os.system('clear')
+                    if "ERROR" in ne.content[0]:
+                        print(ne.content[0])
+                    else:
+                        subprocess.Popen("neurotic")
+                last_run = time.time()
