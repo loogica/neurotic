3aba4f3f1a4aefce77b252274f7801b3173f15e0
Felipe cruz <felipecruz@loogica.net>
2012-12-14 14:25:30 -0200

    raise exception with stdout and stderr in case
    of return code not equal to 0

--- neurotic/async_runner.py
+++ neurotic/async_runner.py
@@ -1,9 +1,18 @@
 import os
 import subprocess
 
+class NeuroticError(Exception):
+    def __init__(self, content):
+        self.content = content
+    def __str__(self):
+        return repr(self.content)
+
 def async_test(command):
     os.chdir(os.getcwd())
     p = subprocess.Popen(command,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
     out, err = p.communicate()
+
+    if not p.returncode == 0:
+        raise NeuroticError((out, err))
