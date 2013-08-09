7de666a4310c801357a4cb82a3b3e6cbd3571207
Felipe cruz <felipecruz@loogica.net>
2013-08-09 16:50:25 -0300

    Show failed tests on web UI

--- neurotic/wsgi_app.py
+++ neurotic/wsgi_app.py
@@ -12,6 +12,45 @@ repository = init_persistent_system(TestReportRepository,
 
 @app.route("/")
 def main():
+    return '''
+<!doctype html>
+<html ng-app>
+  <head>
+    <link href="/static/bootstrap.min.css" rel="stylesheet">
+    <link href="/static/grid.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.6/angular.min.js"></script>
+    <script src="http://underscorejs.org/underscore-min.js"></script>
+    <script src="/static/ace/ace.js" type="text/javascript" charset="utf-8"></script>
+    <script src="/static/neurotic.js"></script>
+    <style type="text/css" media="screen">
+    #editor {
+        height:500px;
+    }
+  </style>
+  </head>
+  <body ng-controller="NeuroticCtrl">
+    <div class="container">
+      <h1>Last Build</h1>
+      <div class="row">
+        <div class="col-6 col-sm-6 col-lg-6">
+          <pre id="editor" ng-show="show">{{ refined_error }}</pre>
+        </div>
+        <div class="col-6 col-sm-6 col-lg-error">
+          <li ng-repeat="test in last_build().reports | filter:failed_filter">
+             <a href="#show-{{ test.id }}" ng-click="show_error(test)">
+               {{ test.outcome }} - {{ test.location[2]  }}
+             </a>
+             <br>
+          </li>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+'''
+
+@app.route("/reports", methods=["GET", "OPTIONS"])
+def reports():
     return jsonify(dict(reports=repository.reports))
 
 if __name__ == "__main__":
